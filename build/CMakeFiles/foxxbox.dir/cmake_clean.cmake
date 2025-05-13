4c7b42be0e2d47fc5d935065816f4c6af9fdbc1c
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-05-13 12:26:18 -0400

    add cmake stuff

---
+++ build/CMakeFiles/foxxbox.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/foxxbox.dir/link.d"
+  "CMakeFiles/foxxbox.dir/src/main.cpp.o"
+  "CMakeFiles/foxxbox.dir/src/main.cpp.o.d"
+  "foxxbox"
+  "foxxbox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/foxxbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
